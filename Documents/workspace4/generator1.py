b5d38cffd35c603471534dda2dda8573786ca5f5
Seol Cheon <jojo432143@gmail.com>
2020-08-23 00:43:41 -0400

    main function in generator1.py added

--- Documents/workspace4/generator1.py
+++ Documents/workspace4/generator1.py
@@ -8,3 +8,27 @@ def getWords(fileName):
     words = tuple(temporaryList)   
 
     return words
+
+def main():
+
+    noun = input("Enter a noun file and extension: ")
+
+    verb = input("\nEnter a verbs file and extension: ")
+    
+    preposition = input("\nEnter a prepositions file and extension: ")
+
+    article = input("\nEnter an artice file and extension: ")    
+
+    article = getWords(article)
+    noun = getWords(noun)
+    verb = getWords(verb)
+    preposition = getWords(preposition)
+
+    print(article)
+    print(noun)
+    print(verb)
+    print(preposition)
+
+if __name__ == "__main__":
+    main()
+
